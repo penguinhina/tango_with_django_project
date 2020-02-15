1e84d33cf9ca6429902292a83018b2e5eae3206b
penguinhina <dorisinthesky@gmail.com>
2020-02-15 15:56:54 +0000

    completed chapter 8,tested OK

--- rango/views.py
+++ rango/views.py
@@ -19,7 +19,9 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
     
 def about(request):
-    return render(request, 'rango/about.html')
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html',{})
     
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -44,11 +46,15 @@ def add_category(request):
        
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
             
     return render(request, 'rango/add_category.html', {'form': form})
+    
+def get_category_list(current_category=None): 
+    return {'categories': Category.objects.all(), 
+            'current_category': current_category}    
     
 def add_page(request, category_name_slug):
      try:
@@ -57,7 +63,7 @@ def add_page(request, category_name_slug):
         category = None
    
      if category is None:
-         return redirect('/rango/')
+         return redirect(reverse('rango:index'))
 
      form = PageForm()
      
